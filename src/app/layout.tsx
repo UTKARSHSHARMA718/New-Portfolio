f848e1a17837ea741696d797b3f8029d63bcec27
Utkarsh-Sharma-79 <utkarsh@astconsulting.in>
2026-03-24 00:53:56 +0530

    feat: added glowing cursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "@/modules/common/components/Navbar";
 import Footer from "@/modules/common/components/Footer";
 import { Poppins } from "next/font/google";
+import GlowCursor from "@/modules/common/components/GlowCursor";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.className} bg-[#0b1020] text-white`}>
         <Navbar />
+        <GlowCursor/>
         {children}
         <Footer />
       </body>
